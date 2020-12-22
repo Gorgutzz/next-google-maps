e631fb2db36c50700ee300b9665863e4c98840cf
leo <leo.kalshteyn@gmail.com>
2020-12-21 16:41:36 -0800

    added currentLocation method for getting input addresses

--- pages/api/geocoding.js
+++ pages/api/geocoding.js
@@ -8,5 +8,15 @@ export default async (req, res) => {
     switch (method) {
       case 'POST':
           try {
+                const currentLocation = await fetch(`https://google-maps-geocoding.p.rapidapi.com/geocode/json?language=en&address=${encodedAddress}`, {
+                    "method": "GET",
+                    "headers": {
+                    "x-rapidapi-host": "google-maps-geocoding.p.rapidapi.com",
+                    "x-rapidapi-key": process.env.RAPIDAPI_KEY
+                    }
+                })
+      default:
+        res.setHeader('Allow', ['POST'])
+        res.status(405).end(`Method ${method} Not Allowed`)
     }
   }
