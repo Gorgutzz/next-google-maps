aaba2026385f4045ef30a4d51041a73150c109a4
leo <leo.kalshteyn@gmail.com>
2020-12-21 16:44:31 -0800

    added lat and long coordinate variables and attractions

--- pages/api/geocoding.js
+++ pages/api/geocoding.js
@@ -15,6 +15,16 @@ export default async (req, res) => {
                     "x-rapidapi-key": process.env.RAPIDAPI_KEY
                     }
                 })
+                const locationData = await currentLocation.json();
+                const lat = locationData.results[0].geometry.location.lat
+                const long = locationData.results[0].geometry.location.lng
+                const attractions = await fetch(`https://tripadvisor1.p.rapidapi.com/attractions/list-by-latlng?lunit=km&currency=USD&limit=10&distance=10&lang=en_US&longitude=${long}&latitude=${lat}`, {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "tripadvisor1.p.rapidapi.com","x-rapidapi-key": process.env.RAPIDAPI_KEY
+                    }
+                })
+        break
       default:
         res.setHeader('Allow', ['POST'])
         res.status(405).end(`Method ${method} Not Allowed`)
