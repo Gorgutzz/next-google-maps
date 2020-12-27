6e77329d41029a75886850fb7a900d966533a68a
leo <leo.kalshteyn@gmail.com>
2020-12-26 16:16:57 -0800

    cusomize the output for coordinate data

--- components/Map.js
+++ components/Map.js
@@ -57,6 +57,26 @@ const Main = () => {
     }
 
     return (
+        <div>
+            <p className={`alert alert-${message.variant}`}>{message.text}</p>
+            <form onSubmit={(e) => updateCoordinates(e)}>
+                <div className="form-group">
+                    <label htmlFor="address">Address</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="address"
+                        required
+                        aria-describedby="addressHelp"
+                        placeholder="42 Wallaby Way, Sydney"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        />
+                    <small id="addressHelp" className="form-text text-muted">The street address that you want to look-up, in the format used by the national postal service of the country concerned. Additional address elements such as business names and unit, suite or floor numbers should be avoided.</small>
+                </div>
+                <button className="btn mb-4 btn-primary" type='submit'>Search Location</button>
+            </form>
+        </div>
     )
 }
 
